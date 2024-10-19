e493b6a474236fcfa3954fdc541be167827a207c
ryulational <97367422+ryulational@users.noreply.github.com>
2024-10-19 16:21:55 +0900

    mix phx.new
    mix phx.new gototravel --no-live --no-mailer

---
+++ gototravel/lib/gototravel_web/controllers/page_controller.ex
@@ -0,0 +1,9 @@
+defmodule GototravelWeb.PageController do
+  use GototravelWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+end
